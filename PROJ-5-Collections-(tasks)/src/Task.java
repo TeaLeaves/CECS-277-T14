df2efdf3953a09e6b8ce58f545c15e93ed4f9f51
Tina L. Vu <TeaLeaves@users.noreply.github.com>
2020-11-08 22:30:56 -0800

    Finished

---
+++ PROJ-5-Collections-(tasks)/src/Task.java
@@ -0,0 +1,108 @@
+/**
+ *  CECS 277 Project 5
+ *  @authors: Matthew Chen, Ryan Ignasiak, Tina Vu
+ *  Due Date: 11/09/20
+ *  Professor Mimi Opkins
+ *  Summary: a program that generates a Task
+ */
+import java.util.*;
+public class Task implements Comparable<Task>{
+    //values
+    int priority; //num from 1-9
+    String description; //task have to do
+
+    /**
+     * default constructor
+     */
+    public Task(){
+        priority = 0;
+        description = "";
+    }
+
+    //argument constructor
+    public Task(int num, String notes){
+        setPriority(num);
+        setDescription(notes);
+    }
+
+    //mutator
+    /**
+     * set the priority with the given param
+     * @param priority the given param, integer from 1-9
+     */
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    /**
+     * set the description with the given param
+     * @param description the task we have to do
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //accessors
+
+    /**
+     * get the priority number of the task
+     * @return the priority number (1-9)
+     */
+    public int getPriority() {
+        return priority;
+    }
+
+    /**
+     * get the description of the task
+     * @return the task description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Override the hashcode to ONLY return description
+     * @return the description of the task
+     */
+    @Override
+    public int hashCode() {
+        return description.hashCode();
+    }
+
+    /**
+     * COMPARE THE DESCRIPTION
+     * @param o the other description we are comparing with
+     * @return true if the description are the same
+     */
+    @Override
+    public boolean equals(Object o) {
+        Task other = (Task) o;
+        //if equals
+        if(other.getDescription().toLowerCase().equals(description.toLowerCase())){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * COMPARE THE PRIORITY NUMBERS
+     * @param o the other task we are comparing with
+     * @return number that represent which is greater
+     */
+    @Override
+    public int compareTo(Task o) {
+        //compare the priority number of this task and the other task
+        //if this priority is greater than the other priority number
+        if(priority > o.getPriority()){
+            return 1;
+        }
+        //if this priority is equal to the other priority number
+        else if(priority == o.getPriority()){
+            return 0;
+        }
+        //if this priority is less than the other priority number
+        else{
+            return -1;
+        }
+    }
+}
---
+++ PROJ-5-Collections-(tasks)/src/ToDoList.java
@@ -0,0 +1,100 @@
+/**
+ *  CECS 277 Project 5
+ *  @authors: Matthew Chen, Ryan Ignasiak, Tina Vu
+ *  Due Date: 11/09/20
+ *  Professor Mimi Opkins
+ *  Summary: a program that generates a To-Do List
+ */
+import java.util.*;
+public class ToDoList {
+    //values
+    PriorityQueue<Task> list = new PriorityQueue();
+
+    //default constructor = new Priority Queue
+    public ToDoList(){
+        PriorityQueue<Task> list = new PriorityQueue();
+    }
+
+    //add tasks ('add' priority(1-9) description)
+    public void addTask(String task){
+        try {
+            //separate the string into a list
+            String[] split = task.split(" ");
+            //create priority integer
+            int priority = Integer.parseInt(split[1]);
+            //create the description string
+            String description = "";
+            for(int i = 2; i < split.length; i++){
+                description = description + split[i] + " ";
+            }
+
+            //length not right
+            if (split.length < 3) {
+                System.out.println("The priority must be an integer between 1 and 9");
+            }
+            //if add is not the first command
+            else if (!split[0].toLowerCase().equals("add")) {
+                System.out.println("The priority must be an integer between 1 and 9");
+            //priority number is not btw 1-9
+            } else if (priority < 1 || priority > 9) {
+                System.out.println("The priority must be an integer between 1 and 9");
+            }
+            else{
+                //create Task(priority, description)
+                Task newTask = new Task(priority, description);
+
+                //array of string diff or same
+                ArrayList<String> answerList = new ArrayList<String>();
+                //check if this task already exist in list
+                for(Task loop: list){
+                    //if description already exist
+                    if(loop.getDescription().equals(description)){
+                        //prints out the hashcode
+                        System.out.println("Task Already Exists: ");
+                        System.out.println("Your Input: " + newTask.hashCode());
+                        System.out.println("Prior Input: " + loop.hashCode());
+                        answerList.add("same description");
+                    }
+                    //if priority number is already used
+                    else if(loop.compareTo(newTask) == 0){
+                        answerList.add("same number");
+                    }
+                    //they are different
+                    else {
+                        answerList.add("diff");
+                    }
+                }
+
+                //if it had the description already
+                if(answerList.contains("same description")){
+                    System.out.print("");
+                }
+                //if the number is already used
+                else if(answerList.contains("same number")){
+                    System.out.println("Priority Number Already Used");
+                }
+                else {
+                    //add task to list
+                    list.add(newTask);
+                }
+            }
+        }
+        //if 2nd command, priority number, is not a number or does not exist
+        catch (NumberFormatException e){
+            System.out.println("The priority must be an integer between 1 and 9");
+        }
+    }
+
+    //next task (removes and prints most urgent task)
+    public void nextTask(){
+        //if list is NOT empty
+        if(list.size() > 0){
+            System.out.println(list.poll().getDescription());
+        }
+        //else list is empty
+        else{
+            System.out.println("There are no tasks in the list.");
+        }
+    }
+
+}
